fbf5eacded52c3f29b41ede9c452e554ec06490f
思月行云 <kenkao_01@126.com>
2018-02-01 10:01:58 +0800

    main

---
+++ nets/INetWorker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace itfantasy.gun.nets
+{
+    public interface INetWorker
+    {
+	    void Connect(string url, string tag);
+        error Send(byte[] msg);
+        error SendAsync(byte[] msg, Action<bool> callback = null);
+	    error BindEventListener(INetEventListener eventListener);
+        void Close();
+        void Update();
+    }
+}
